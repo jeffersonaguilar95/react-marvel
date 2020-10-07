e539bef0f45c60096e899dbda6b24960806f8897
Jefferson <jefferson.aguilar95@gmail.com>
2020-10-07 01:40:02 -0300

    feat(App): Initial Layout
    Material UI initial configuration

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Router, RouteComponentProps } from "@reach/router"
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import Layout from './Layout/Layout'
+
+let Home = (props: RouteComponentProps) => <div>Home</div>;
+let Dash = (props: RouteComponentProps) => <div>Dash</div>;
+
+const theme = createMuiTheme();
+
+function  App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Router>
+          <Home path="/" />
+          <Dash path="dashboard" />
+        </Router>
+      </Layout>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/app/Layout/Layout.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import {
+	Drawer,
+	AppBar,
+	Toolbar,
+	List,
+	CssBaseline,
+	Typography,
+	Divider,
+	IconButton,
+	ListItem,
+	ListItemIcon,
+	ListItemText
+} from '@material-ui/core';
+import {
+	Menu as MenuIcon,
+	ChevronLeft as ChevronLeftIcon,
+	ChevronRight as ChevronRightIcon,
+	MoveToInbox as InboxIcon,
+	Mail as MailIcon
+} from '@material-ui/icons';
+import useStyles from './useStyles'
+
+const Layout: React.FC = ({ children}) => {
+	const classes = useStyles();
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<AppBar
+				position="fixed"
+				className={clsx(classes.appBar, {
+					[classes.appBarShift]: open,
+				})}
+			>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						onClick={handleDrawerOpen}
+						edge="start"
+						className={clsx(classes.menuButton, {
+							[classes.hide]: open,
+						})}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" noWrap>
+						React Marvel - Jefferson
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				variant="permanent"
+				className={clsx(classes.drawer, {
+					[classes.drawerOpen]: open,
+					[classes.drawerClose]: !open,
+				})}
+				classes={{
+					paper: clsx({
+						[classes.drawerOpen]: open,
+						[classes.drawerClose]: !open,
+					}),
+				}}
+			>
+				<div className={classes.toolbar}>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					{['Inbox', 'Starred'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{['All mail'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
+			<main className={classes.content}>
+				{children}
+			</main>
+		</div>
+	);
+}
+
+export default Layout;
