4d40fdbfd5256d1fc42457c02ee1ccb984feb319
Jefferson <jefferson.aguilar95@gmail.com>
2020-10-11 13:26:32 -0300

    fix: eslint improvements

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -13,6 +13,7 @@ const useWidth = (): Breakpoint => {
   const keys: Breakpoint[] = [...theme.breakpoints.keys].reverse()
   return (
     keys.reduce((output: BreakpointOrNull, key: Breakpoint) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const matches = useMediaQuery(theme.breakpoints.up(key))
       return !output && matches ? key : output
     }, null) || 'xs'
