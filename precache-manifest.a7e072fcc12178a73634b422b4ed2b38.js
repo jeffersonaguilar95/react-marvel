8c5c761850753cff02973b395df4f6835e538f6f
Jefferson <jefferson.aguilar95@gmail.com>
2020-10-11 23:36:58 -0300

    Deploy

---
+++ precache-manifest.a7e072fcc12178a73634b422b4ed2b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64549b8eee67ffeccc4cbb14cd759f29",
+    "url": "/react-marvel/index.html"
+  },
+  {
+    "revision": "5141e53809f8dd674633",
+    "url": "/react-marvel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1500e44eafda2e10dad0",
+    "url": "/react-marvel/static/js/2.413cdd78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-marvel/static/js/2.413cdd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5141e53809f8dd674633",
+    "url": "/react-marvel/static/js/main.8ddb22a0.chunk.js"
+  },
+  {
+    "revision": "a10a515008031a84d26a",
+    "url": "/react-marvel/static/js/runtime-main.00f8431d.js"
+  }
+]);
